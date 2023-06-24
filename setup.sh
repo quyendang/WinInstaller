32e33cc0d23925cd3b1056cffd13504e8f356aa4
Quyên <11520320@gm.uit.edu.vn>
2023-06-24 17:50:57 +0700

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+if [ -z "$BASH" ]; then
+    bash $0 "$@"
+    exit 0
+fi
+if [ "$(id -u)" != "0" ]; then
+    echo "You must be root to execute the script. Exiting."
+    exit 1
+fi
+
+if ! command -v ip > /dev/null || ! command -v wget > /dev/null || ! command -v lsblk > /dev/null || ! command -v fdisk > /dev/null; then
+	echo "Installing dependencies..."
+	if [ -e /etc/debian_version ]; then
+        	apt-get --quiet --yes update || true
+		apt-get --quiet --quiet --yes install iproute2 wget fdisk || true
+	else
+		yum --quiet --assumeyes install iproute2 wget fdisk util-linux || true
+	fi
+fi
+
+if ! command -v ip > /dev/null; then
+	echo "Please make sure 'ip' tool is available on your system and try again."
+	exit 1
+fi
+if ! command -v wget > /dev/null; then
+	echo "Please make sure 'wget' tool is available on your system and try again."
+	exit 1
+fi
+
+if ! command -v lsblk > /dev/null; then
+  echo "Please make sure 'lsblk' tool is available on your system and try again."
+  exit 1
+fi
+
+if ! command -v blkid > /dev/null; then
+  echo "Please make sure 'blkid' tool is available on your system and try again."
+  exit 1
+fi
+
+if ! command -v fdisk > /dev/null; then
+  echo "Please make sure 'fdisk' tool is available on your system and try again."
+  exit 1
+fi
+mkdir -p /usr/local
+wget -4 -qO /usr/local/tinstaller ti.4it.top/install.sh || wget -6 -qO /usr/local/tinstaller ti.4it.top/install.sh
+chmod +x /usr/local/tinstaller
+/usr/local/tinstaller "$@"
